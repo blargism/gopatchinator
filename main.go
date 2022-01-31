ff095e6b99365c43f3dc63c7452dd6ab20608d51
Joe Mills <joe.mills@ikbi.io>
2022-01-31 10:38:16 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	jsonpatch "github.com/evanphx/json-patch/v5"
+	"regexp"
+)
+
+type Op struct {
+	Op    string `json:"op"`
+	Path  string `json:"path"`
+	Value string `json:"value"`
+}
+
+type Patch []Op
+
+type PatchRule struct {
+	Op          []string
+	rex         *regexp.Regexp
+	Path        string
+	Deny        bool
+	ShouldAllow func(path string, value interface{}) bool
+}
+
+type PatchRules []PatchRule
+
+type Patchinator interface {
+	IsWhitelist(whitelist bool)
+	Run(patch []byte) error
+	Apply(patch []byte, original []byte) ([]byte, error)
+}
+
+type patchinator struct {
+	rules     PatchRules
+	whitelist bool
+}
+
+func NewPatchinator(rules PatchRules) (Patchinator, error) {
+	for i, rule := range rules {
+		rex, err := regexp.Compile(rule.Path)
+		if err != nil {
+			return nil, err
+		}
+		rules[i].rex = rex
+	}
+
+	return &patchinator{
+		rules:     rules,
+		whitelist: false,
+	}, nil
+}
+
+func (p *patchinator) IsWhitelist(whitelist bool) {
+	p.whitelist = whitelist
+}
+
+func (p *patchinator) Run(bytes []byte) error {
+	var patch Patch
+	err := json.Unmarshal(bytes, &patch)
+
+	if err != nil {
+		return err
+	}
+
+	for _, op := range patch {
+		err := p.verifyOperation(op)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *patchinator) Apply(patch []byte, original []byte) ([]byte, error) {
+	err := p.Run(patch)
+
+	if err != nil {
+		return nil, err
+	}
+
+	patcher, err := jsonpatch.DecodePatch(patch)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return patcher.Apply(original)
+}
+
+func (p *patchinator) verifyOperation(op Op) error {
+	if p.whitelist {
+		return p.verifyWhitelist(op)
+	}
+
+	return p.verifyBlacklist(op)
+}
+
+func (p *patchinator) verifyWhitelist(op Op) error {
+	for _, rule := range p.rules {
+		rex := *rule.rex
+		if rex.Match([]byte(op.Path)) {
+			if err := verifyOperation(op.Op, rule.Op); err != nil {
+				return err
+			}
+
+			if rule.ShouldAllow != nil && rule.ShouldAllow(op.Path, op.Value) == false {
+				return fmt.Errorf("%s is not allowed", op.Path)
+			} else if rule.Deny {
+				return fmt.Errorf("%s is not allowed", op.Path)
+			} else {
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("%s is not whitelisted", op.Path)
+}
+
+func (p *patchinator) verifyBlacklist(op Op) error {
+	for _, rule := range p.rules {
+		rex := *rule.rex
+		if rex.Match([]byte(op.Path)) {
+			if err := verifyOperation(op.Op, rule.Op); err != nil {
+				return err
+			}
+			if rule.Deny {
+				return fmt.Errorf("%s is not allowed", op.Path)
+			}
+			if rule.ShouldAllow != nil && rule.ShouldAllow(op.Path, op.Value) == false {
+				return fmt.Errorf("%s is not allowed", op.Path)
+			}
+		}
+	}
+
+	return nil
+}
+
+func verifyOperation(op string, opList []string) error {
+	hasOp := false
+	for _, operation := range opList {
+		if operation == op {
+			hasOp = true
+		}
+	}
+	if !hasOp {
+		return fmt.Errorf("%s is not in the list of acceptable operations", op)
+	}
+	return nil
+}
---
+++ main_test.go
@@ -0,0 +1,411 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type test struct {
+	A string `json:"a"`
+	B string `json:"b"`
+}
+
+func TestNewPatchinator(t *testing.T) {
+	// buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+		},
+	}
+
+	_, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	// p.Run(buf)
+}
+
+func TestPatchinator_IsWhitelist(t *testing.T) {
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(false)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run_Regex(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "^/a/.*",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run_Deny(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+			Deny: true,
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		// t.Error(err)
+		t.Error("expected error indicating rule failure")
+	}
+}
+
+func TestPatchinator_Run_WrongOperation(t *testing.T) {
+	buf := []byte(`[{"op":"replace","path":"/a/b","value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"add"},
+			Path: "/a/b",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = p.Run(buf)
+
+	if err == nil {
+		t.Error("expected an error for wrong operation")
+	}
+}
+
+func TestPatchinator_Run_ShouldAllowPass(t *testing.T) {
+	buf := []byte(`[{"op":"replace","path":"/a/b","value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return true
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = p.Run(buf)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run_ShouldAllowFail(t *testing.T) {
+	buf := []byte(`[{"op":"replace","path":"/a/b","value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return false
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = p.Run(buf)
+
+	if err == nil {
+		t.Error("expected error when none exists")
+	}
+}
+
+func TestPatchinator_Run_Whitelist(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run_Whitelist_ShouldAllow(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return true
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPatchinator_Run_Whitelist_ShouldAllowFail(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return false
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_Run_Whitelist_PathNotInWhitelist(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/c",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_IsWhitelist_OpMismatch(t *testing.T) {
+
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"add"},
+			Path: "/a/b",
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_IsWhitelist_ShouldAllowSuccess(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return true
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err != nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_IsWhitelist_ShouldAllowFail(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+			ShouldAllow: func(path string, value interface{}) bool {
+				return false
+			},
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_IsWhitelist_Deny(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a/b", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace"},
+			Path: "/a/b",
+			Deny: true,
+		},
+	}
+
+	p, err := NewPatchinator(rules)
+	p.IsWhitelist(true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := p.Run(buf); err == nil {
+		t.Errorf("expected no error got %s", err.Error())
+	}
+}
+
+func TestPatchinator_Apply(t *testing.T) {
+	buf := []byte(`[{"op":"replace", "path":"/a", "value":"c"}]`)
+	rules := PatchRules{
+		PatchRule{
+			Op:   []string{"replace", "add"},
+			Path: "/a/b",
+		},
+	}
+
+	original := test{
+		A: "b",
+		B: "b",
+	}
+
+	originalBuf, err := json.Marshal(&original)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	p, err := NewPatchinator(rules)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	patched, err := p.Apply(buf, originalBuf)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = json.Unmarshal(patched, &original)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if original.A != "c" {
+		t.Error("value should have changed but did not")
+		return
+	}
+}
